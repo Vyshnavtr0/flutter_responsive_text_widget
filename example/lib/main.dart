b22162ad80655301c4f8761665e08d5002bdd642
Vyshnavtr0 <vyshnavtr0@gmail.com>
2024-07-10 14:11:14 +0530

    initail version

---
+++ example/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_text/responsive_text.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ResponsiveText(
+            style: TextStyle(color: Colors.black),
+            maxTextSize: 50,
+            text:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          )
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/responsive_text.dart
@@ -0,0 +1,77 @@
+library responsive_text;
+
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class ResponsiveText extends StatefulWidget {
+  String text;
+  double? maxTextSize;
+  double? minTextSize;
+  TextStyle? style;
+  double? scale = 0.05;
+
+  ResponsiveText(
+      {super.key,
+      required this.text,
+      this.maxTextSize,
+      this.minTextSize,
+      this.style,
+      this.scale});
+
+  @override
+  State<ResponsiveText> createState() => _ResponsiveTextState();
+}
+
+class _ResponsiveTextState extends State<ResponsiveText> {
+  @override
+  Widget build(BuildContext context) {
+    // Get the screen width
+    double parentWidth = MediaQuery.of(context).size.width;
+    double parentHeight = MediaQuery.of(context).size.height;
+
+    // Use the smaller of the two dimensions for calculation
+    double parentSize = parentWidth < parentHeight ? parentWidth : parentHeight;
+
+    // Calculate the text size based on the parent width
+    double scale = widget.scale ?? 0.05;
+    double calculatedTextSize =
+        parentSize * scale; // Adjust the multiplier as needed
+
+    // Set default min and max text size if not provided
+    double minTextSize = widget.minTextSize ?? 12.0;
+    double maxTextSize = widget.maxTextSize ?? 24.0;
+
+    // Clamp the text size within the min and max range
+    double textSize = calculatedTextSize.clamp(minTextSize, maxTextSize);
+
+    return SizedBox(
+      child: Text(
+        widget.text,
+        style: TextStyle(
+          inherit: widget.style?.inherit ?? true,
+          color: widget.style?.color,
+          fontWeight: widget.style?.fontWeight,
+          fontStyle: widget.style?.fontStyle,
+          letterSpacing: widget.style?.letterSpacing,
+          wordSpacing: widget.style?.wordSpacing,
+          textBaseline: widget.style?.textBaseline,
+          height: widget.style?.height,
+          locale: widget.style?.locale,
+          foreground: widget.style?.foreground,
+          background: widget.style?.background,
+          shadows: widget.style?.shadows,
+          fontFeatures: widget.style?.fontFeatures,
+          decoration: widget.style?.decoration,
+          decorationColor: widget.style?.decorationColor,
+          decorationStyle: widget.style?.decorationStyle,
+          decorationThickness: widget.style?.decorationThickness,
+          debugLabel: widget.style?.debugLabel,
+          fontFamily: widget.style?.fontFamily,
+          fontFamilyFallback: widget.style?.fontFamilyFallback,
+          overflow: widget.style?.overflow,
+          fontSize: textSize,
+        ),
+      ),
+    );
+  }
+}
